14e002c26ec1d87f178b9ef2a91517a4165ff0b1
Indrajit Sen Gupta <indrajitsg@gmail.com>
2021-05-30 23:10:41 +0530

    Added script and config files

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# cowin
-Identify available slots on cowin.gov.in website for COVID 19 vaccination
+# Check Vaccine Availability on Cowin Website
+In this repo we have a Python script to identify available slots on cowin.gov.in website for COVID 19 vaccination. Note that the script currently checks for availabilty of first dose only. If the user wants to change it to second dose, it can be done with minimal changes to the script. The user needs to specify the **state name**, **pincode** and **age** in the yaml.config file. Parameter **district** can be ignored as it is not used by the **main** function. To run the program, specify the date and the number of days ahead to search. If the program finds any availability, it will play an alarm. The mp3 file added in this repo is from https://soundbible.com/1937-Tornado-Siren-II.html which is under Attribution 3.0 license. It can be replaced by any desired mp3 the user wants - after making the necessary changes in the **main** function inside cowin_check.py.
+
+### Packages to be Installed
+- cowin_api
+- yaml
+- playsound
+
+### Instructions
+1. In your Python environment ensure the packages mentioned above are installed.
+2. Open config.yaml in any editor like Notepad and specify the parameters.
+3. From command line, to check for availability for two consecutive days run the python script cowin_api as follows
+```
+python cowin_check.py -d 25-05-2021 -n 1
+```
+
---
+++ cowin_check.py
@@ -0,0 +1,166 @@
+import sys, getopt
+import datetime
+import time
+import numpy
+from cowin_api import CoWinAPI
+import playsound
+import signal
+import yaml
+
+#------------------------------------------
+# Helper functions
+#------------------------------------------
+
+# Read config parameters file
+def get_config_param():
+    """Read config file for params"""
+    with open('config.yaml') as con:
+        config = yaml.safe_load(con)
+    return config
+
+
+# Search State ID
+def find_state_id(api):
+    """Find state id given a state name"""
+    config = get_config_param()
+
+    all_states = api.get_states()['states']
+    for state in all_states:
+        if state['state_name'] == config['state']:
+            return state['state_id']
+
+# find_state_id(api=cowin)
+
+
+# Check valid district
+def check_valid_district(api):
+    """Find whether a district name is valid and returns id"""
+    config = get_config_param()
+    state_id = find_state_id(api=api)
+    districts = api.get_districts(state_id=state_id)['districts']
+    found = False
+    found_id = 0
+    for district in districts:
+        if district['district_name'] == config['district']:
+            found = True
+            found_id = district['district_id']
+    return found, found_id
+
+# check_valid_district(api=cowin)
+
+
+# Get centers available by district name
+def check_avail_district(api, check_date):
+    """Get centers available by district name"""
+    config = get_config_param()
+    dist_found, dist_id = check_valid_district(api=api,
+                                               state_name=config['state'],
+                                               district_name=config['district'])
+    if dist_found is False:
+        print("Incorrect state name or district name")
+        return None
+    else:
+        available_centers = api.get_availability_by_district(str(dist_id),
+                                                             check_date,
+                                                             config['age'])
+        return available_centers
+
+# check_avail_district(api=cowin, check_date="25-05-2021")
+
+
+# Get centers available by pincode
+def check_avail_pincode(api, check_date):
+    """Get center availability by pincode"""
+    config = get_config_param()
+    try:
+        available_centers_by_pin = api.get_availability_by_pincode(config['pincode'],
+                                                                   check_date,
+                                                                   config['age'])
+    except:
+        available_centers_by_pin = {'centers': []}
+    return available_centers_by_pin
+
+# check_avail_pincode(api=cowin, check_date="25-05-2021")
+
+
+# Perform a search for specific days
+def search(api, start, days_to_check):
+    """Search a set a of days for availability"""
+    config = get_config_param()
+    start_dt = datetime.datetime.strptime(start, "%d-%m-%Y")
+    print("-----------------------------------------")
+    for day in range(0, days_to_check+1):
+        time_delta = datetime.timedelta(day)
+        new_dt = start_dt + time_delta
+        new_date = new_dt.strftime("%d-%m-%Y")
+
+        print("Checking for:", new_date)
+        response = check_avail_pincode(api=api,
+                                       check_date=new_date)
+
+        if len(response['centers']) > 0:
+            num_centers = len(response['centers'])
+            for j in range(num_centers):
+                center_name = response['centers'][j]['name']
+                center_available_capacity = response['centers'][j]['sessions'][0]['available_capacity_dose1']
+                if center_available_capacity > 0:
+                    print(f"Center found at {center_name} with available capacity {center_available_capacity}")
+                    return True
+    return False
+
+#------------------------------------------
+# Main function
+#------------------------------------------
+# Handle exits safely
+def signal_handler(signal, frame):
+    global exit_now
+    print("Exiting program")
+    exit_now = True
+
+def main(argv):
+    """Start a search from specified date for the next n days"""
+    start_date = ""
+    days = 0
+    signal.signal(signal.SIGINT, signal_handler)
+    global exit_now
+    exit_now = False
+
+    # Check parameters
+    try:
+        opts, args = getopt.getopt(argv, "d:n:", ["date=", "numdays="])
+    except getopt.GetoptError:
+        print("Use the following format:")
+        print("python cowin_check.py -d 17-05-2021 -n 3")
+        sys.exit()
+    
+    for opt, arg in opts:
+        if opt in ("-d", "--date"):
+            start_date = arg
+        if opt in ("-n", "--numdays"):
+            days = int(arg)
+    
+    # Initial display message
+    print("You have specified: ")
+    print("Starting date:", start_date)
+    print("Number of days to check:", days+1)
+
+    # Start the check
+    cowin = CoWinAPI()
+    starttime = time.time()
+    response = False
+    while response is False:
+        response = search(api=cowin, start=start_date, days_to_check=days)
+        # Delay for 60 seconds
+        if not response:
+            time.sleep(15.0 - ((time.time() - starttime) % 15.0))
+        if exit_now:
+            break
+    if response is True:
+        print("-------------------")
+        print("Book immediately!!!")
+        print("-------------------")
+        playsound.playsound("audio/Tornado_Siren_II-Delilah-747233690.mp3")
+    return None
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
